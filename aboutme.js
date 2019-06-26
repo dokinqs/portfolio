7b76bbac9c7e56cbbca04f64d64ad15ce7ea38c3
Karen Zhao <karen.zhao18@gmail.com>
2019-06-26 01:20:14 -0400

    change theme

--- aboutme.js
+++ aboutme.js
@@ -1,95 +1,82 @@
-/////////////// GREETING ALERT ///////////////
-
-// document.addEventListener("DOMContentLoaded", function() { 
-//   console.log("DOM parsed"); 
-// });
-
-// window.onload = function() {
-//   var date = new Date();
-//   alert(`Hey, you found me. C: - ${date.toLocaleString('en-US')}`);
-// }
-
-
 /////////////// CHANGE PIC ///////////////
 
 
-var imgx =  document.getElementById('selfpic');
+// let imgx =  document.getElementById('selfpic');
 
-function changePic() {
-  imgx.src = (imgx.getAttribute('src') === "./img/art.jpg") ? "./img/pic.jpg" : "./img/art.jpg";
-}
+// function changePic() {
+//   imgx.src = (imgx.getAttribute('src') === "./img/art.jpg") ? "./img/pic.jpg" : "./img/art.jpg";
+// }
 
 
 /////////////// STICKY NAVBAR ///////////////
 
-var navig = document.getElementById("navbar");
-var topOfNav = navig.offsetTop;
+let navig = document.getElementById("navbar");
+let topOfNav = navig.offsetTop;
 
 function fixNav() {
   if (window.scrollY >= topOfNav) {
-    document.body.style.paddingTop = navig.offsetHeight + 'px';
+    // document.body.style.paddingTop = navig.offsetHeight + 'px';
+    document.body.style.marginTop = navig.offsetHeight + 'px'
     navbar.classList.add("fixed-nav");
   } else {
     navbar.classList.remove("fixed-nav");
-    document.body.style.paddingTop = 0;
+    // document.body.style.paddingTop = 0;
+    document.body.style.marginTop = 0;
   }
 }
-
 window.addEventListener('scroll', fixNav);
 
-// ES6
+// or ES6
 // const navig = document.querySelector('#navbar');
 // let topOfNav = navig.offsetTop;
+
 // window.onscroll = () => fixNav();
 
 // if forced reflow: resized window, refresh
-// dokinqs was here
-// no swiping man
-// nah to ES6/ css vendor prefix/ browser support
 
 /////////////// GET SCROLL POSITION (Ref Only) ///////////////
 
 
 // for early IE7, rect phys px only
 function GetZoomFactor() {
-    var factor = 1;
+    let factor = 1;
     if (document.body.getBoundingClientRect) {
-        var rect = document.body.getBoundingClientRect();
-        var physicalW = rect.right - rect.left;
-        var logicalW = document.body.offsetWidth;
+        let rect = document.body.getBoundingClientRect();
+        let physicalW = rect.right - rect.left;
+        let logicalW = document.body.offsetWidth;
         factor = Math.round((physicalW / logicalW) * 100) / 100;
     }
     return factor;
 }
 
-// refresh to get scroll postiions of doc when zoom level is not 1
+// refresh to get scroll positions of doc when zoom level is not 1
 function GetScrollPositions() {
     if ('pageXOffset' in window) {
-        // var scrollLeft =  window.pageXOffset;
-        var scrollTop = window.pageYOffset;
+        // let scrollLeft =  window.pageXOffset;
+        let scrollTop = window.pageYOffset;
     } else {      
     // for IE7
-        var zoomFactor = GetZoomFactor();
-        // var scrollLeft = Math.round(document.documentElement.scrollLeft / zoomFactor);
-        var scrollTop = Math.round(document.documentElement.scrollTop / zoomFactor);
+        let zoomFactor = GetZoomFactor();
+        // let scrollLeft = Math.round(document.documentElement.scrollLeft / zoomFactor);
+        let scrollTop = Math.round(document.documentElement.scrollTop / zoomFactor);
     }
     // console.log("X scroll amount: " + scrollLeft + "px");
-    if (scrollTop < 3000) {
-      console.log("Y scroll amount: " + scrollTop + "px");
-    } else {
-      // desktop view near bottom
-      console.log("%cY scroll amount: " + scrollTop + "px", "color: pink; font-size: large");
-    }
+
+    // if (scrollTop < 3000) {
+    //   console.log("Y scroll amount: " + scrollTop + "px");
+    // } else {
+    //   // desktop view near bottom
+    //   console.log("%cY scroll amount: " + scrollTop + "px", "color: pink; font-size: large");
+    // }
+
     // if (scrollTop >= 3984) { 
-    //     alert('~ Do you wanna go back up? ~ ');
-    //     // idk bottom px for 12"+ screens.. should calc instead
-    //     // TODO throttle, change %down and message
+
     // }
 }
 
 window.addEventListener('scroll', GetScrollPositions);
 
-// var timeout;
+// let timeout;
 // window.addEventListener('scroll', function() {
   // clearTimeout(GetScrollPositions);
   // if (!timeout) {
@@ -101,25 +88,38 @@ window.addEventListener('scroll', GetScrollPositions);
 // }, false);
 
 
+/////////////// GREETING ALERT ///////////////
+
+
+// document.addEventListener("DOMContentLoaded", function() { 
+//   console.log("DOM parsed"); 
+// });
+
+// window.onload = function() {
+//   let date = new Date();
+//   alert(`Hey, you found me. C: - ${date.toLocaleString('en-US')}`);
+// }
+
+
 /////////////// MARQUEE ///////////////
 
 
-var hi = "d o k i n q s";
-document.getElementById('banner').innerHTML = "Made with ♡ by « " + hi + " »"; 
+// let hi = "d o k i n q s";
+// document.getElementById('banner').innerHTML = "Made with ♡ by « " + hi + " »"; 
 
 
 /////////////// CAROUSEL ///////////////
 
 
 // (function() {
-//     var box = document.querySelector('.carousel');
-//     var next = box.querySelector('.next');
-//     var prev = box.querySelector('.prev');
+//     let box = document.querySelector('.carousel');
+//     let next = box.querySelector('.next');
+//     let prev = box.querySelector('.prev');
   
-//     var counter = 0;
-//     var items = box.querySelectorAll('.slides li');
-//     var amount = items.length;
-//     var current = items[0];
+//     let counter = 0;
+//     let items = box.querySelectorAll('.slides li');
+//     let amount = items.length;
+//     let current = items[0];
   
 //     box.classList.add('active');
   
@@ -154,9 +154,9 @@ document.getElementById('banner').innerHTML = "Made with ♡ by « " + hi + " »
 /////////////// BACK TO TOP ///////////////
 
 
-(function() {
-  var teleport = document.querySelector('.teleport');
-  teleport.addEventListener('click', function() { 
-    alert('If you want to fly, you have to let go of what weighs you down.'); 
-  });
-})();
+// (function() {
+//   let teleport = document.querySelector('.teleport');
+//   teleport.addEventListener('click', function() { 
+//     alert('If you want to fly, you have to let go of what weighs you down.'); 
+//   });
+// })();
